73c227bd12d56ab15cd40aa0e6d954a604b3bc75
Thomas Nixon <thomas@devika.com>
2022-07-18 17:03:11 +1000

    Improve output on rename script

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -2,19 +2,19 @@
 
 echo "Changing project to [$1]"
 
-sed -i '' -e "s|baseline-webpage|$1|g" serverless.yml
+sed -i '' -e "s|baseline-webpage|$1|g" serverless.yml >/dev/null 2>&1
 echo 'Updated [serverless.yml]'
 
-sed -i '' -e "s|baseline-webpage|$1|g" package.json
+sed -i '' -e "s|baseline-webpage|$1|g" package.json >/dev/null 2>&1
 echo 'Updated [package.json]'
 
-sed -i '' -e "s|baseline-webpage|$1|g" .github/workflows/deploy.yml
+sed -i '' -e "s|baseline-webpage|$1|g" .github/workflows/deploy.yml >/dev/null 2>&1
 echo 'Updated [.github/workflows/deploy.yml]'
 
-sed -i '' -e "s|baseline-webpage|$1|g" scripts/setup-aws.sh
+sed -i '' -e "s|baseline-webpage|$1|g" scripts/setup-aws.sh >/dev/null 2>&1
 echo 'Updated [scripts/setup-aws.sh]'
 
-sed -i '' -e "s|baseline-webpage|$1|g" scripts/rename.sh
+sed -i '' -e "s|baseline-webpage|$1|g" scripts/rename.sh >/dev/null 2>&1
 echo 'Updated [scripts/rename.sh]'
 
 echo "Done"
