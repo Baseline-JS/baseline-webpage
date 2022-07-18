b6d5c779f4052365ad7b62e7c6f9f298647e00b0
Thomas Nixon <thomas@devika.com>
2022-07-18 14:17:52 +1000

    Improved setup-aws-profile.sh

--- scripts/setup-aws-profile.sh
+++ scripts/setup-aws-profile.sh
@@ -1,17 +1,41 @@
 #!/usr/bin/env bash
 
-printf "Setup an AWS profile for [\033[32m$1\033[39m]\n"
+# Sets REGION, PROFILE, AWS_REGION, AWS_PROFILE
+. ./scripts/project-variables.sh
 
-echo "Enter the AWS Access Key: "
-read AWS_ACCESS_KEY_ID
-echo "Enter the AWS Secret Key: "
-read -s AWS_SECRET_ACCESS_KEY
-echo "AWS Session Token (optional): "
-read -s AWS_SESSION_TOKEN
+printf "Setup an AWS profile for [\033[32m%s\033[39m]\n" "$1"
+
+printf "Enter the AWS Access Key: "
+read -r AWS_ACCESS_KEY_ID
+printf "Enter the AWS Secret Key: "
+read -sr AWS_SECRET_ACCESS_KEY
+printf "\nAWS Session Token (optional): "
+read -sr AWS_SESSION_TOKEN
+printf "\n"
 
 echo "Configuring AWS...."
 # If you specify a profile with --profile on an individual command, that overrides the setting specified in the environment variable for only that command.
-aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID:-}" --profile $1
-aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY:-}" --profile $1
-aws configure set aws_session_token "${AWS_SESSION_TOKEN:-}" --profile $1
+aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID:-}" --profile "$AWS_PROFILE"
+aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY:-}" --profile "$AWS_PROFILE"
+if [ "$AWS_SESSION_TOKEN" ]; then
+    echo "Setting session token"
+    aws configure set aws_session_token "${AWS_SESSION_TOKEN:-}" --profile "$AWS_PROFILE"
+else
+    EXISTING_SESSION=$(aws configure get aws_session_token --profile "$AWS_PROFILE")
+    if [ "$EXISTING_SESSION" ]; then
+        echo "Existing session token found, removing"
+        aws configure set aws_session_token "" --profile "$AWS_PROFILE"
+    else
+        echo "No existing session token... ignoring"
+    fi
+fi
+
+echo "Testing AWS Keys..."
+IAM_RESULT=$(aws iam list-users --profile "$AWS_PROFILE")
+if [ "$IAM_RESULT" ]; then
+    echo "Credentials work!"
+else
+    echo "AWS Keys did not work!"
+fi
+
 echo "Done"
